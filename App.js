5660d9433a4633027fd815e083af5d22c45bb317
parkson-tano <tanoparksonsilencer@gmail.com>
2023-01-06 00:28:06 +0100

    prebuild

--- App.js
+++ App.js
@@ -1,19 +1,20 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Button, View, Text, SafeAreaView, RefreshControl } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import RegisterStack from "./src/components/navigation/RegisterStack";
-import { AppRegistry } from "react-native";
 import Service from "./src/components/auth/Services";
 import { AuthProvider } from "./src/components/context/AuthContext";
 import { AxiosProvider } from "./src/components/context/AxiosContext";
 import DashboardStack from "./src/components/navigation/DashboardStacjk";
-import MyTabs from "./src/components/MyTabs";
-import Dashboard from "./src/components/Dashboard";
-
+import * as SplashScreen from "expo-splash-screen";
 const Stack = createNativeStackNavigator();
 
+SplashScreen.hideAsync();
+
 function App() {
+  useEffect(() => {
+    SplashScreen.hideAsync();
+  }, []);
   return (
     <AuthProvider>
       <AxiosProvider>
